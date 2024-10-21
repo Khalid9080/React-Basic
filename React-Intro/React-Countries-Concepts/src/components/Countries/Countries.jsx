6d2ebbda2cd9a36c65e0ccf5667b336fd87ff9cb
Khalid9080 <khalidsaifullah9080@gmail.com>
2024-10-21 14:23:02 +0600

    Countries Flags

--- React-Intro/React-Countries-Concepts/src/components/Countries/Countries.jsx
+++ React-Intro/React-Countries-Concepts/src/components/Countries/Countries.jsx
@@ -2,10 +2,15 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    //To show The flags --
+    const [flags, setFlags] = useState([]);
+
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -14,12 +19,69 @@ const Countries = () => {
 
     }, [])
 
+    const handleVisitedCountry = (country_prameter) => { //s1
+        console.log('add this to ypur visited country list', country_prameter); //s2
+        const newVisitedCountries = [...visitedCountries, country_prameter];  //R1
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flag_prameter => { 
+        const newVisitedFlags = [...flags, flag_prameter];
+        setFlags(newVisitedFlags);
+    }
+
+/* S
+   --> 1. Amra prottek Mark Visited Button a click korle, Country gulo page a list akare show korte cai. er jonno handleVisitedCountry name ekta arrow function declear kora hoise.
+   --> 2. er jonno, 2nd step holo, Coutries.jsx er vitor ekta massage likha jate eta console a ekta masg show korae.
+   --> 3. 2nd step holo, handleVisitedCountry arrow function k props akare Country.jsx file a pathanor jonno ekat props creat kora.Country tag er vitor.(handleVisitedCountry_props)
+   --> 4. Country.jsx file er main Country arrow function/Component er vitpor handleVisitedCountry_props function er props add kora. --> go to Country.jsx file
+   --> 5. passWithParams name r ekta arrow function create korta , then er vitor handleVisitedCountry_props function call kora. function er paramenter hishebe country_props pass kora.
+   --> 6. passWithParams function ta, Mark visited button a call kora hoise. onk rokom vabe call kora jae.
+*/
+
+/* ---visited country set korar jonno-- R
+    --> koto gulo country visit korsi eta dekhar jonno spred operator use korte hobe, spreead operator ager visited coutry gulo k array banaia copy kore rakhe,
+        then, setVisitedCountries(newVisitedCountries); er maddhome setVisitedCountries a newVisitedCountries array ta pass kore dite hobe.
+    --> return er vitor visitedCountries.map() use kore {visitedCountry.name.common} coutry gulor obj die dite hobe.
+*/
+
+
+
     return (
+
         <div>
             <h3>Countries: {countries.length}</h3>
+        {/* Visited Coutries */}
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        // R2
+                        visitedCountries.map(visitedCountry => 
+                        <li key={visitedCountry.cca3}>
+                        {visitedCountry.name.common}</li>)
+                    }
+                   
+                </ul>
+            </div>
+
+            <div className="flag-container">
+                {
+                    flags.map(flag => <img src={flag} alt="" />)
+                }
+
+            </div>
+
+            {/* Display Countries */}
+            <div className="country-container">
             {
-                countries.map(cntry => <Country key={cntry.cca3} country_props={cntry}> </Country> )
+                countries.map(cntry => <Country key={cntry.cca3}
+                     handleVisitedCountry_props={handleVisitedCountry}
+                     handleVisitedFlags_props={handleVisitedFlags}
+                      country_props={cntry}> </Country> )
             }
+            </div>
+          
         </div>
     );
 };
--- React-Intro/React-Countries-Concepts/src/components/Country/Country.jsx
+++ React-Intro/React-Countries-Concepts/src/components/Country/Country.jsx
@@ -1,12 +1,52 @@
-
+import { useState } from 'react';
 import './Country.css';
 
-const Country = ({country_props}) => {
-    const {name,flags} =country_props;
+const Country = ({ country_props,handleVisitedCountry_props,handleVisitedFlags_props }) => { //s4
+
+    const { name, flags, population, area, cca3 } = country_props;
+    // visit korsi ki korinai etar ekta flag thakbe, default flag thakbe false, visit korinai.
+
+    const [visited,setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited); // true o use kora jay.
+    }
+
+
+  
+
+    /*
+    evabe likha jae,
+    const passWithParams = () => {
+        handleVisitedCountry_props(country_props);
+    or,
+    const passWithParams = () =>handleVisitedCountry_props(country_props);
+
+    **or,
+      <button onClick={() =>handleVisitedCountry_props(country_props)}>Visited </button>
+    }
+
+    */
+
     return (
-        <div className='country'>
-            <h4>Name : {name?.common}</h4>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>  
+
+            {/* Conditional CSS */}
+            <h4 style={{color : visited ? 'purple' : 'white'}}>Name : {name.common}</h4> 
             <img src={flags.png} alt="" />
+            <p>Populations : {population}</p>
+            <p>Area : {area}</p>
+            <p><small>Code : {cca3}</small></p>
+
+            <button onClick={()=>handleVisitedCountry_props(country_props)}>Mark visited</button>
+            <br />
+            <button onClick={()=>handleVisitedFlags_props(country_props.flags.png)}>Add Flag</button>
+
+            <button onClick={handleVisited}>Visited </button>
+            {visited && 'I have vsited this country.'}
+
+            {/* visited ? 'I have vsited this country.' : 'I want to visit' */}
+
         </div>
     );
 };
